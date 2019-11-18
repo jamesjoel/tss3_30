ed90c542eb93f2dfa35d051fc96e19e845ed7d31
jamesjoel <james.joelj@gmail.com>
2019-11-18 17:10:21 +0530

    modules in NodeJS

---
+++ james/nodejs/day1/demo.js
@@ -0,0 +1,10 @@
+module.exports=function(){
+    return {
+        name : "rohit",
+        age : 25,
+        city : "indore",
+        calc : function(a, b){
+            return a+b;
+        }
+    }
+}
