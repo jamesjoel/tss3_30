ec78782a2380e5e43cf01df1a71f83ad757103d4
jamesjoel <james.joelj@gmail.com>
2019-11-25 18:16:05 +0530

    login and signup in node

--- james/project/app.js
+++ james/project/app.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var app = express();
 
+
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+
+// var n=0;
+// var arr = ["/about", "/", "/contact"];
+// app.use(arr, function(req, res, next){
+//     n++;
+//     console.log("Total : ",n);
+//     // console.log(req.url);
+//     // console.log(req.originalUrl);
+//     next();
+// });
 
 app.get("/", function(req, res){
     var pagedata = { pagename : "home/index", title : "Home" };
@@ -13,14 +25,22 @@ app.get("/about", function (req, res) {
     var pagedata = { pagename: "about/index", title : "About" };
     res.render("layout", pagedata);
 });
-app.get("/contact", function (req, res) {
-    var pagedata = { pagename: "contact/index", title : "Contact" };
+app.get("/login", function (req, res) {
+    var pagedata = { pagename: "login/index", title: "Login" };
+    res.render("layout", pagedata);
+});
+app.get("/signup", function (req, res) {
+    var pagedata = { pagename: "signup/index", title: "Signup" };
     res.render("layout", pagedata);
 });
 
 
 
 
+app.get("/contact", function (req, res) {
+    var pagedata = { pagename: "contact/index", title : "Contact" };
+    res.render("layout", pagedata);
+});
 app.listen(3000, function(){
     console.log("server running");
 });
