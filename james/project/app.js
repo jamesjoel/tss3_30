59816a2f493a213e1758dc46cb44ed31eb46cc73
jamesjoel <james.joelj@gmail.com>
2019-11-26 17:29:19 +0530

    routing in express

--- james/project/app.js
+++ james/project/app.js
@@ -1,10 +1,26 @@
 var express = require("express");
 var app = express();
-
+var routes = require("./config/routes");
 
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+
+app.use(routes);
+
+app.listen(3000, function(){
+    console.log("server running");
+});
+
+
+
+
+
+
+
+
+
+
 
 // var n=0;
 // var arr = ["/about", "/", "/contact"];
@@ -15,32 +31,3 @@ app.use(express.static(__dirname+"/public"));
 //     // console.log(req.originalUrl);
 //     next();
 // });
-
-app.get("/", function(req, res){
-    var pagedata = { pagename : "home/index", title : "Home" };
-    res.render("layout", pagedata);
-});
-
-app.get("/about", function (req, res) {
-    var pagedata = { pagename: "about/index", title : "About" };
-    res.render("layout", pagedata);
-});
-app.get("/login", function (req, res) {
-    var pagedata = { pagename: "login/index", title: "Login" };
-    res.render("layout", pagedata);
-});
-app.get("/signup", function (req, res) {
-    var pagedata = { pagename: "signup/index", title: "Signup" };
-    res.render("layout", pagedata);
-});
-
-
-
-
-app.get("/contact", function (req, res) {
-    var pagedata = { pagename: "contact/index", title : "Contact" };
-    res.render("layout", pagedata);
-});
-app.listen(3000, function(){
-    console.log("server running");
-});
