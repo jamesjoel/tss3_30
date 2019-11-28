5ae9e4b0e9bbaaed67ce111f1e536f0e84b4df6d
jamesjoel <james.joelj@gmail.com>
2019-11-28 17:05:58 +0530

    change action for signup

--- james/project/controllers/signup.js
+++ james/project/controllers/signup.js
@@ -5,7 +5,7 @@ routes.get("/", function (req, res) {
     var pagedata = { pagename: "signup/index", title: "Signup" };
     res.render("layout", pagedata);
 });
-routes.post("/save", function(req, res){
+routes.post("/", function(req, res){
     console.log(req.body);
 });
 
