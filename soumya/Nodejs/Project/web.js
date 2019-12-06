98802c311d3e012394e790b07d0950d12a2a97ee
Soumya8989 <57490665+Soumya8989@users.noreply.github.com>
2019-12-07 01:27:35 +0530

    work on Login Authentication

--- soumya/Nodejs/Project/web.js
+++ soumya/Nodejs/Project/web.js
@@ -1,12 +1,20 @@
 var express = require("express");
-var parser = require("body-parser");
+var bodyParser = require("body-parser");
 var routes = require("./config/routes");
 var web = express();
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var flash = require("express-flash");
 
-web.use(parser());
 
 web.set("view engine", "ejs");
 web.use(express.static(__dirname + "/public"));
+
+web.use(bodyParser());
+web.use(cookieParser());
+web.use(session({ secret: "soumya3000" }));
+web.use(flash());
+
 web.use(routes);
 
 web.listen(3000, function() {
