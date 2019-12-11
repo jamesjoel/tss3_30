6d35ebbf470179d45a6e9efa9da2b657f804760c
Soumya8989 <57490665+Soumya8989@users.noreply.github.com>
2019-12-11 21:11:39 +0530

    admin login authentication

--- soumya/Nodejs/Project/config/admin_router.js
+++ soumya/Nodejs/Project/config/admin_router.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var routes = express.Router();
+var url = ["/dashboard", "/product", "/category"];
+
+routes.use(url, function(req, res, next) {
+
+
+    if (!req.session.is_admin_logged_in) {
+        res.redirect("/admin");
+        return;
+    }
+    next();
+});
 
 routes.use("/product", require("../controllers/admin/product"));
 routes.use("/", require("../controllers/admin/login"));
+routes.use("/category", require("../controllers/admin/category"));
+routes.use("/dashboard", require("../controllers/admin/dashboard"));
+
+
+routes.get("/logout", function(req, res) {
+    req.session.destroy();
+    res.redirect("/");
+});
+
 module.exports = routes;
