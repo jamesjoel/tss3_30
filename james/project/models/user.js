4378e273a9f931dde40293851a8e50efa1e343a0
jamesjoel <james.joelj@gmail.com>
2019-12-20 17:33:45 +0530

    change password and shopping cart

--- james/project/models/user.js
+++ james/project/models/user.js
@@ -13,3 +13,9 @@ module.exports.search=function(where, cb){
         db.collection("user").find(where).toArray(cb);
     });
 }
+module.exports.update = function(where, obj, cb){
+    connect(function (err, client) {
+        var db = client.db(database.dbName);
+        db.collection("user").update(where, { $set : obj}, cb);
+    });
+}
