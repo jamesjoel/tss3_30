198d47a108e81950e776e66b0e1f166762fb8171
Soumya8989 <Soumyaagrawal8989@gmail.com>
2019-12-25 03:42:15 +0530

    work on buy product

--- soumya/Nodejs/Project/controllers/MoreInfo/moreinfo.js
+++ soumya/Nodejs/Project/controllers/MoreInfo/moreinfo.js
@@ -2,7 +2,8 @@ var express = require("express");
 var routes = express.Router();
 var Product = require("../../models/product");
 var mongodb = require("mongodb");
-
+var Buynow = require("../../models/buynow");
+var Product = require("../../models/product");
 routes.get("/:id",function(req,res){
     // console.log(req.params);
     var id = req.params.id;
@@ -17,12 +18,43 @@ Product.check({_id: mongodb.ObjectId(id)},function(err,result){
 });
 });
 
-routes.get("/buynow",function(req,res){
-    var pagedata = {
-        pagename: "MoreInfo/congrats",
-        title: "Successfully"
+routes.get("/buynow/:id",function(req,res){
+    // console.log(req.body);
+    var productid = req.params.id;
+    var userid = req.session.id;
+    
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    
+    req.body.product_id = productid;
+    req.body.user_id = userid;
+    req.body.date = date;
+    req.body.time = time;
+    
+    if(req.session.is_user_logged_in){
+        
+        Buynow.save(req.body,function(err,result){
+            Product.check({_id: mongodb.ObjectId(productid)},function(err,result){
+
+                var pagedata = {
+                    pagename: "MoreInfo/congrats",
+                    title: "Successfully",
+                    product_name: result[0].product_title,
+
+                }
+                
+                res.render("layout",pagedata);
+            });
+        });
+    }else{
+        req.session.buymsg = req.flash("msg");
+        req.flash("msg","If You Want To Buy This Product,You Must First Login ")
+        res.redirect("/login");
     }
-    res.render("layout",pagedata);
+
 });
 
 
