4098682dc4b2e87f678d197debd7f84e52fd5532
jamesjoel <james.joelj@gmail.com>
2019-12-24 17:41:02 +0530

    client side JS

--- james/project/controllers/admin/product.js
+++ james/project/controllers/admin/product.js
@@ -10,32 +10,34 @@ var changename = require("../../helpers/changename");
 var fs = require("fs");
 
 
-
 routes.get("/", function (req, res) {
+    // "localhost:3000/admin/product" for show add product page
     Category.search({}, function(err, result){
+        //getting all category for show category dropdown
         var pagedata = { title: "Product", pagename: "admin/product/index", category : result, errorMsg : req.flash("msg") };
         res.render("admin_layout", pagedata);
-
+        
     });
 });
 
 
 
-
 routes.post("/", function(req, res){
+    // "localhost:3000/admin/product" for getting form data 
     // console.log(req.files);
     var image = req.files.image;
     var size = image.size;
 
 
     var imgData = changename(image.name);
+    // calling changename helper for change and set unique name of each uploaded file
 
     var ext = imgData[1];
     var newName = imgData[0];
 
 
     var uploadPath = path.resolve()+"/public/upload/"+newName;
-
+    // for set complete upload path to upload the file
     if(ext =="jpg" || ext == "gif" || ext == "png" || ext == "jpeg")
     {
         if(size <= (1024*1024))
@@ -75,6 +77,7 @@ routes.post("/", function(req, res){
 });
 
 routes.get("/view", function(req, res){
+    // localhost:3000/admin/product/view to show all added product
     Product.search({}, function(err, result){
         var pagedata = { title: "View Product", pagename: "admin/product/view", product : result, errorMsg : req.flash("msg") };
         res.render("admin_layout", pagedata); 
@@ -82,7 +85,7 @@ routes.get("/view", function(req, res){
     });
 });
 routes.get("/delete/:id", function(req, res){
-    // console.log(req.query);
+    // localhost:3000/admin/product/delete/ID for get unique id and delete it from DB
     var a = req.params.id;
 
     // fs.unlink();
@@ -90,7 +93,10 @@ routes.get("/delete/:id", function(req, res){
         // console.log(result);
         var imgname = result[0].image;
         var delPath = path.resolve() + "/public/upload/" +imgname;
+    // for set complete deleted path to delete a file
+
         fs.unlink(delPath, function(err){
+            // for delete file by unlink fun
             Product.delete({ _id: mongodb.ObjectId(a) }, function (err, result) {
                 
                 res.redirect("/admin/product/view");
@@ -102,6 +108,7 @@ routes.get("/delete/:id", function(req, res){
     
 });
 routes.get("/edit/:id", function(req, res){
+    // localhost:3000/admin/product/edit/ID to show specific product for edit its info
     var a = req.params.id;
     Category.search({}, function(err, result1){
 
@@ -114,6 +121,7 @@ routes.get("/edit/:id", function(req, res){
 });
 
 routes.post("/update", function(req, res){
+    // for UPDATE product data to DB
     var id = req.body.id;
     delete req.body.id;
     Product.update({ _id : mongodb.ObjectId(id)}, req.body, function(err, result){
@@ -123,6 +131,7 @@ routes.post("/update", function(req, res){
 
 
 routes.post("/upload", function(req, res){
+    // for update specific product image in DB and Folder also
     var id = req.body.id;
     var image = req.files.image;
     var size = image.size;
