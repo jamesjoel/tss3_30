f990ae430556f04439d6cdd9af11ddd1e44f9194
jamesjoel <james.joelj@gmail.com>
2020-01-07 18:26:38 +0530

    socket day1

---
+++ james/socket1/app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+/*var socketIo = require("socket.io");
+var io = socketIo(server);*/
+
+var io = require("socket.io")(server);
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname+"/public"));
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+io.on("connection", function(socket){
+    // console.log("------", socket.id);
+    // socket.on("disconnect", function(){
+    //     console.log("server disconnect with socket id", socket.id);
+    // });
+
+    socket.on("send", function(data){
+        io.emit("sendToClient", data);
+        
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(3000, function(){
+    console.log("server running");
+});
