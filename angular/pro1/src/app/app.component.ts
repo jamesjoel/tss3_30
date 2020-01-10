657ec617d058ee5776c30d6d6b2ebaf221a31c6b
jamesjoel <james.joelj@gmail.com>
2020-01-10 17:27:37 +0530

    angular day 3

--- angular/pro1/src/app/app.component.ts
+++ angular/pro1/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template : "<h1>hello</h1>",
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- angular/pro1/src/app/app.module.ts
+++ angular/pro1/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoxComponent } from './box/box.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoxComponent
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
