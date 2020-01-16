76ad5b5a263d37bc8ef3f81441178fa1d1262c13
jamesjoel <james.joelj@gmail.com>
2020-01-16 18:24:19 +0530

    parent to child day 1

---
+++ angular/pro1/src/app/shared/delete/delete.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Student } from '../../model/student.interface';
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.scss']
+})
+export class DeleteComponent implements OnInit {
+
+  @Input() cSelectedStudent:Student;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
