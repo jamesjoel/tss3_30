751395fd4f7c1eddd861405f4c5c1f3c083b06d4
Soumya8989 <Soumyaagrawal8989@gmail.com>
2020-02-10 15:18:25 +0530

    fileUpload

--- soumya/Nodejs/Project/controllers/admin/slider.js
+++ soumya/Nodejs/Project/controllers/admin/slider.js
@@ -7,12 +7,12 @@ var fs = require("fs");
 var mongodb = require("mongodb");
 
 routes.get("/", function (req, res) {
-    Slider.check({},function(err,result){
+    Slider.check({}, function (err, result) {
 
         var pagedata = {
             pagename: "admin/slider/index",
             title: "Slider",
-            slider: result ,
+            slider: result,
             errMsg: req.flash("msg")
         }
         res.render("admin_layout", pagedata);
@@ -36,7 +36,7 @@ routes.post("/", function (req, res) {
                     res.redirect("/admin/slider");
                 });
             });
- 
+
         } else {
             req.flash("msg", "This File Size is Too Large");
             res.redirect("/admin/slider");
@@ -48,7 +48,7 @@ routes.post("/", function (req, res) {
 });
 
 routes.post("/uploadslider", function (req, res) {
-   
+
     var id = req.body.id;
     var image = req.files.image;
     var size = image.size;
@@ -56,33 +56,33 @@ routes.post("/uploadslider", function (req, res) {
     var imgname = return_value[0];
     var ext = return_value[1];
     var UploadPath = path.resolve() + "/public/Slider/" + imgname;
-    if (ext == "jpg"|| ext == "jpeg"|| ext == "png"|| ext == "gif") {
-        if (size <= 1024*1024) {
-            Slider.check({_id : mongodb.ObjectId(id)},function(err,result){
-               var OldImageName = result[0].image;
-               var DelPath = path.resolve()+"/public/Slider/"+OldImageName;
-               fs.unlink(DelPath,function(err){
+    if (ext == "jpg" || ext == "jpeg" || ext == "png" || ext == "gif") {
+        if (size <= 1024 * 1024) {
+            Slider.check({ _id: mongodb.ObjectId(id) }, function (err, result) {
+                var OldImageName = result[0].image;
+                var DelPath = path.resolve() + "/public/Slider/" + OldImageName;
+                fs.unlink(DelPath, function (err) {
 
-                   image.mv(UploadPath,function(err){
-                           Slider.update({_id: mongodb.ObjectId(id)},{image : imgname},function(err,result){
-                               res.redirect("/admin/slider");
-                           });
-                   });
-               });
+                    image.mv(UploadPath, function (err) {
+                        Slider.update({ _id: mongodb.ObjectId(id) }, { image: imgname }, function (err, result) {
+                            res.redirect("/admin/slider");
+                        });
+                    });
+                });
             });
 
         } else {
             req.flash("msg", "This File is Too Large");
             res.redirect("/admin/slider");
         }
-    }else{
+    } else {
         req.flash("msg", "This File Type not allowed");
         res.redirect("/admin/slider");
     }
 });
 
 routes.get("/delete/:id", function (req, res) {
-    var id = req.params.id; 
+    var id = req.params.id;
     Slider.check({ _id: mongodb.ObjectId(id) }, function (err, result) {
         // console.log(result);
         var imagename = result[0].image;
